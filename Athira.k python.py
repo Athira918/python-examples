55039b98d79788a7d4b84b488954473e6a971839
Athira918 <k18athira@gmail.com>
2024-07-25 15:14:35 +0530

    python basic examples

---
+++ Athira.k python.py
@@ -0,0 +1,85 @@
+
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+sum=a+b
+c=None
+c
+type(c)
+name=input("enter the name" )
+name
+type(name)
+
+age=float(input("enter the age "))
+age
+type(age)
+print('Name :',name,'\b\bAge :',age )
+print("Name:%r Age:%d"%(name))
+print("Name:{0} Age:{0}".format(name))
+print(f"Name:{name} Age:{age}")
+numbers=2,3.4,5,3+4j,2.1,"come",True,
+numbers
+type(numbers)
+x=(2,3.4,5,3+4j,2.1,"come",True,1 )
+x
+type(x)
+dir(x)
+x.index(2)
+x.count(2.1)
+x.count(1)
+m=[2,3.4,5,3+4j,2.1,"come",True,1 ]
+m
+type(m)
+dir(m)
+dir(name)
+m.index(2)
+m.count(5)
+m.count(1)
+m.append(x)
+m[-1]
+m.extend(x)
+m.insert(4,10)
+m.pop()
+m
+i=m
+i
+j=m.copy()
+j
+id(m)
+id(i)
+id(j)
+m.pop()
+m.reverse()
+m.sort()
+m
+k=[4,6,3,9,7.13,9,28,22,20]
+k
+type(k)
+k.sort()
+g=["h","o","u","n"]
+g.sort(reverse=True)
+g
+g.remove("h")
+g.append("h")
+g.clear()
+u={"name":"athira","age":21,"place":"mangode","dob":"18/9/2003"}
+u
+type(u)
+dir(u)
+u["name"]
+u.keys()
+u.items()
+u.values()
+u["mark"]=50
+u.update({"name":"muba","age":20})
+u.pop("dob")
+u
+t=[6,7,9,7,6,7,9]
+t
+type(t)
+p=set(t)
+p
+type(p)
+dir(p)
+e={5,9,10,5,9,7}
+e
+type(e)
